cbe4edabfade0477e9e40a8cf79507fa96017501
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2020-04-05 20:37:57 +0200

    Add project files.

---
+++ BlazorRenderFragmentPositionIssue/Components/ListItem.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorRenderFragmentPositionIssue.Components
+{
+  public class ListItem : ComponentBase, IDisposable
+  {
+    [CascadingParameter]
+    public List ParentList { get; set; }
+
+    [Parameter]
+    public string Value { get; set; }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+      if (firstRender)
+      {
+        ParentList.AddItem(Value);
+      }
+    }
+
+    public void Dispose()
+    {
+      ParentList.RemoveItem(Value);
+    }
+  }
+}
